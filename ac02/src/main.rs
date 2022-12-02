98fbe1a40c5b37efbb0dad7ffc1bb5f7ae361415
adam bie <adam.bie@tutanota.com>
2022-12-02 17:50:07 +0100

    2nd day done

--- ac02/src/main.rs
+++ ac02/src/main.rs
@@ -8,20 +8,75 @@ fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
 
-fn main() {
-    let mut cals: Vec<i64> = vec![];
+fn getResult (a: char, b: char) -> i32  {
+    let mut res: i32 = 0;
 
-    let lines = lines_from_file("01.txt").expect("Could not load lines");
-    let mut sum: i64 = 0;
-    for line in lines {
-        if line == "" {
-            cals.push(sum);
-            sum = 0;
-        } else {
-            sum += line.parse::<i64>().unwrap();
-        }
+    if b == 'X' {   //rock
+        if a == 'A' { res = 4; }
+        else if a == 'B' { res = 1;}
+        else if a == 'C' { res = 7;};
     }
-    cals.sort();
-    println!("top is {}", cals[cals.len()-1]);
-    println!("top3 are {}", cals[cals.len()-1] + cals[cals.len()-2] + cals[cals.len()-3]);
+
+    if b == 'Y' {   //paper
+        if a == 'A' { res = 8;}
+        else if a == 'B' { res = 5; }
+        else if a == 'C' { res = 2; };
+    }
+
+    if b == 'Z' {   //scissors
+        if a == 'A' { res = 3; }
+        else if a == 'B' { res = 9; }
+        else if a == 'C' { res = 6; };
+    }    
+
+    res
+}
+
+fn getResult2 (a: char, b: char) -> i32  {
+    let mut res: i32 = 0;
+
+
+    //rock
+    //paper
+    //scissors
+
+    if b == 'X' {   //loose
+        if a == 'A' { res = 3; }
+        else if a == 'B' { res = 1;}
+        else if a == 'C' { res = 2;};
+    }
+
+    if b == 'Y' {   //draw
+        if a == 'A' { res = 4;}
+        else if a == 'B' { res = 5; }
+        else if a == 'C' { res = 6; };
+    }
+
+    if b == 'Z' {   //win
+        if a == 'A' { res = 8; }
+        else if a == 'B' { res = 9; }
+        else if a == 'C' { res = 7; };
+    }    
+
+    res
+}
+
+
+fn main() {
+    let mut first: char;
+    let mut second :char;
+    let mut score: i32 = 0;
+    let mut score2: i32 = 0;
+
+
+    let lines = lines_from_file("ac02.txt").expect("Could not load lines");
+    for line in lines {
+        first = line.chars().nth(0).unwrap();
+        second = line.chars().nth(2).unwrap();
+
+        score += getResult(first, second);
+        score2 += getResult2(first, second);
+    }
+    println!("score is {}", score);
+    println!("2nd score is {}", score2);
 }
