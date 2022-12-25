b56e891c7ccb8a4e486951e39ab4baafb6961d34
adam bie <adam.bie@tutanota.com>
2022-12-25 18:58:36 +0100

    day 20 done

--- ac20/src/main copy.rs
+++ ac20/src/main copy.rs
@@ -2,15 +2,14 @@ use std::{
     fs::File,
     io::{self, BufRead, BufReader},
     path::Path,
-    collections::HashMap,
 };
 fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct CodeNumber {
-    number: i32,
+    number: i64,
     place: usize
 }
 
@@ -18,74 +17,40 @@ fn main() {
 
     let lines = lines_from_file("20.txt").expect("Could not load lines");
     let mut numbers: Vec<CodeNumber> = Vec::new();
+
     for (idx, line) in lines.iter().enumerate() {
         numbers.push(
             CodeNumber {
-                number: line.parse::<i32>().unwrap(),
+                number: line.parse::<i64>().unwrap(),
                 place: idx                  
             }
         );
     }  
 
-    // for (idx, number) in numbers.iter().enumerate() {
-    //     println!("{:?} at pos {}",number,idx);
-    // }
+    let mut numbers2 = numbers.clone();
 
     //-------------------------------part1
-    let list_len = numbers.len();
+    let list_len: i64 = (numbers.len()-1 ) as i64;
 
     for current in 0..numbers.len() {
-        // println!("moving item {}", current);
         let curr_idx = numbers.iter().position(|x| x.place == current ).unwrap();
-        // println!("curridx:{}", curr_idx);
-        let mut new_idx: i32 = curr_idx as i32 + numbers[curr_idx].number ;
-        // println!("newidx:{}", new_idx);
-        if numbers[curr_idx].number < 0{
-            new_idx -= 1;
-        } else if new_idx>list_len as i32{
-            new_idx += 1;
-        }
-        // println!("moving item {} pos {} into {}",numbers[curr_idx].number, current, new_idx);
+        
+        let mut new_idx: i64 = curr_idx as i64 + numbers[curr_idx].number ;
         let temp = numbers.remove(curr_idx);
-        if new_idx>0 {
-        loop {
-            if new_idx>list_len as i32 {
-                new_idx -= list_len as i32;
-            } else {
-                break;
-            } 
-        }
-        // println!("recalcidx:{}",new_idx);
+        
+        new_idx = ((new_idx % list_len) + list_len) % list_len;
+        numbers.insert( new_idx as usize, temp);
 
-        } else if new_idx<0 {
-            loop {
-                if new_idx<0  {
-                    new_idx += list_len as i32;
-                } else {
-                    break;
-                } 
-            }   
-        }
-        if new_idx<=numbers.len() as i32 {
-            numbers.insert(new_idx as usize, temp);
-        } else {
-            numbers.push(temp);
-        }
-        // println!("after");
-        // for (idx, number) in numbers.iter().enumerate() {
-        //     println!("{:?} at pos {}",number,idx);
-        // }
     }
 
     //get sum of items at 1000 2000 3000 pos
     let results = vec![1000,2000,3000];
     let startpos =  numbers.iter().position(|x| x.number == 0 ).unwrap();
-    println!("val 0 pos {}",startpos);
-    println!("{:?}",numbers[(startpos + 1000) as usize]);
-    println!("{:?}",numbers[(startpos + 2000) as usize]);
-    println!("{:?}",numbers[(startpos + 3000) as usize]);
+    println!("val {:?} pos {}",numbers[startpos], startpos);
+
+    let mut sum=0;
     for result in results {
-        let mut idx = result + startpos;
+        let mut idx = ((result + startpos) as i64 % list_len) as usize;
         if idx>numbers.len(){
             //idx += 1;
             loop {
@@ -97,7 +62,50 @@ fn main() {
             }
         }
         println!("res: {:?}", numbers[idx]);
+        sum += numbers[idx].number;
 
     }
+    println!("sum of part1: {}", sum);
+
+    //-------------------------------part2
+    for x in 0..numbers2.len() {
+        numbers[x].number *= 811589153;
+    }
+
+    for _ in 0..10 {
+        for current2 in 0..numbers2.len() {
+            let curr_idx = numbers2.iter().position(|x| x.place == current2 ).unwrap();
+            
+            let mut new_idx: i64 = curr_idx as i64 + numbers2[curr_idx].number ;
+            let temp = numbers2.remove(curr_idx);
+            
+            new_idx = ((new_idx % list_len) + list_len) % list_len;
+            numbers2.insert( new_idx as usize, temp);
+    
+        }        
+    }
+    //get sum of items at 1000 2000 3000 pos
+    let results = vec![1000,2000,3000];
+    let startpos =  numbers.iter().position(|x| x.number == 0 ).unwrap();
+    println!("val {:?} pos {}",numbers[startpos], startpos);
+
+    let mut sum=0;
+    for result in results {
+        let mut idx = ((result + startpos) as i64 % list_len) as usize;
+        if idx>numbers.len(){
+            //idx += 1;
+            loop {
+                if idx>numbers.len(){
+                    idx -= numbers.len();
+                } else {
+                    break;
+                }
+            }
+        }
+        println!("res: {:?}", numbers[idx]);
+        sum += numbers[idx].number;
+
+    }
+    println!("sum of part1: {}", sum);
 
 }
--- ac20/src/main.rs
+++ ac20/src/main.rs
@@ -7,9 +7,9 @@ fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct CodeNumber {
-    number: i32,
+    number: i64,
     place: usize
 }
 
@@ -17,27 +17,60 @@ fn main() {
 
     let lines = lines_from_file("20.txt").expect("Could not load lines");
     let mut numbers: Vec<CodeNumber> = Vec::new();
+    let mut numbers2: Vec<CodeNumber> = Vec::new();
     for (idx, line) in lines.iter().enumerate() {
         numbers.push(
             CodeNumber {
-                number: line.parse::<i32>().unwrap(),
+                number: line.parse::<i64>().unwrap(),
+                place: idx                  
+            }
+        );
+        numbers2.push(
+            CodeNumber {
+                number: line.parse::<i64>().unwrap(),
                 place: idx                  
             }
         );
     }  
 
-    //-------------------------------part1
-    let list_len: i32 = (numbers.len()-1 ) as i32;
+    // let mut numbers2 = numbers.clone();
 
-    for current in 0..numbers.len() {
-        let curr_idx = numbers.iter().position(|x| x.place == current ).unwrap();
-        
-        let mut new_idx: i32 = curr_idx as i32 + numbers[curr_idx].number ;
-        let temp = numbers.remove(curr_idx);
-        
-        new_idx = ((new_idx % list_len) + list_len) % list_len;
-        numbers.insert( new_idx as usize, temp);
+    let res1 = calculate(numbers,1,1); 
+    println!("part1 res: {}",res1);
 
+    let res2 = calculate(numbers2,10,811589153); 
+    println!("part2 res: {}",res2);
+
+
+
+
+    // //-------------------------------part2
+    // for x in 0..numbers2.len() {
+    //     numbers[x].number *= 811589153;
+    // }
+
+  
+
+}
+
+fn calculate(mut numbers: Vec<CodeNumber>, times: i32, mult: i64)-> i64 {
+    
+    let list_len: i64 = (numbers.len()-1 ) as i64;
+    for x in 0..numbers.len() {
+        numbers[x].number *= mult;
+    }
+
+    for _ in 0..times {
+        for current in 0..numbers.len() {
+            let curr_idx = numbers.iter().position(|x| x.place == current ).unwrap();
+            
+            let mut new_idx: i64 = curr_idx as i64 + numbers[curr_idx].number ;
+            let temp = numbers.remove(curr_idx);
+            
+            new_idx = ((new_idx % list_len) + list_len) % list_len;
+            numbers.insert( new_idx as usize, temp);
+
+        }
     }
 
     //get sum of items at 1000 2000 3000 pos
@@ -47,7 +80,7 @@ fn main() {
 
     let mut sum=0;
     for result in results {
-        let mut idx = ((result + startpos) as i32 % list_len) as usize;
+        let mut idx = ((result + startpos) as i64 % list_len) as usize;
         if idx>numbers.len(){
             //idx += 1;
             loop {
@@ -62,11 +95,6 @@ fn main() {
         sum += numbers[idx].number;
 
     }
-    println!("sum of part1: {}", sum);
-
-    //-------------------------------part2
-
-
-
-    
+    // println!("sum of part1: {}", sum);
+    sum
 }
